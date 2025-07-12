df6f681c35b2b1652fc16b00fe6b7e8a3af66933
Emanuel Lázaro <emanuellzr01@outlook.com>
2025-07-11 22:23:35 -0300

    feat: adicionar DATABASE_URL e SSL em api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -10,11 +10,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Obtém todos os livros do banco de dados.
